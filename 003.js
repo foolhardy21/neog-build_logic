b0ce44949a72672794997e689d4f23613e623f52
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-01-11 15:10:16 +0530

    4 questions complete

---
+++ 003.js
@@ -0,0 +1,15 @@
+//1
+const add = (a,b) => a+b
+console.log(add(20,40))
+
+//2
+const SI = (p,r,t) => (p*r*t)/100
+console.log(SI(100,6,2))
+
+//3
+const KE = (m,v) => 0.5*m*v*v
+console.log(KE(10,1))
+
+//4
+const toCelsius = fahrenheit => (fahrenheit-32)*5/9
+console.log(toCelsius(56))
