34874305fc16eb84be7d4c0e985523392fe6875d
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-01-12 18:29:39 +0530

    004 till intermediate

--- 004.js
+++ 004.js
@@ -48,3 +48,44 @@ const countVowelsConsonants = str => ({
 })
 console.log(countVowelsConsonants('apple'))
 
+//9
+const checkLength = str => str.length > 7 ? true : false
+console.log(checkLength('assjsjsjs'))
+
+//10
+const copyStr = (big, small) => big+small
+console.log(appendStr('asdasdas','ooo')) 
+
+//11
+const ifPalindrome = str => (str === str.split('').reverse().join(''))
+console.log(ifPalindrome('hannaha'))
+
+//12
+const substringsArr = str => {
+    let substrArr = []
+    for(let i=0; i<str.length; i++) {
+        for(let j=i; j<str.length+1; j++) {
+            substrArr.push(str.slice(i,j)) 
+        }
+    }
+    return substrArr.filter(str => str.length>0)
+}
+console.log(substringsArr('vinay'))
+
+//13
+const removeTime = str => str.slice(0,-5)
+console.log(removeTime('Wed April 15, 7pm'))
+
+//14
+const maskStr = str => str.split('').map((char, index) => {
+    if(index < str.length-4) return '#'
+    else return char
+}).join('')
+console.log(maskStr('5565534276455423'))
+
+//15
+const firstSixCapital = str => str.split('').map((char, index) => {
+        if(index <= 6) return char.toUpperCase()
+        else return char
+    }).join('')
+console.log(firstSixCapital('tic tac toe is a fun game'))
