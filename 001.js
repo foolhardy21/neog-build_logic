8c74f42dda7caac5813cd6d28ceb333bb1dbc7d8
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-01-12 14:52:56 +0530

    001 complete

--- 001.js
+++ 001.js
@@ -43,3 +43,77 @@ const checkDays = (month) => {
     console.log(new Date(2022, months.indexOf(month)+1, 0).getDate())
 }
 // check0Days('Feb')
+
+
+//INTERMEDIATE
+//1
+const fizzBuzz = () => {
+    let arr = []
+    for(let i = 1; i<=100; i++) {
+        if(i%3 === 0 && i%5 === 0) {
+            arr.push('FizzBuzz')
+        } else if(i%3 === 0) {
+            arr.push('Fizz')
+        } else if(i%5 === 0) {
+            arr.push('Buzz')
+        } else arr.push(i)
+    }
+    return arr
+}
+// console.log(fizzBuzz())
+
+//2
+const starPattern = () => {
+    for(let i = 1; i<=5; i++) {
+        let str = ''
+        for(let j = 1; j<=i; j++) {
+            str+='*'
+        }
+        console.log(str)
+    }
+}
+// starPattern()
+
+//3
+const tableTillTwelve = num => {
+    for(let i = 1; i <=12; i++) {
+        console.log(`${num}x${i} = ${num*i}`)
+    }
+}
+// tableTillTwelve(readline.question('Enter the number'))
+
+//4
+const fibonacci = (num) => {
+    // let num1=0
+    // const num2 = 
+    let arr = [0,1]
+    let i = 1
+    let pos1 = 0, pos2 = 1
+    while(i++ <= num) {
+        arr.push(arr[pos1]+arr[pos2])
+        pos1++
+        pos2++
+    }
+    return arr
+}
+// console.log(fibonacci(10))
+
+const ifPrime = num => {
+    for(let i = 2; i<=Math.sqrt(num); i++) {
+        if(num%i === 0) {
+            return false
+        }
+    }
+    return true
+}
+// console.log(ifPrime(18))
+
+const ifWeekday = day => {
+    switch(day) {
+        case 'Sunday':
+        case 'Saturday': return `${day} is weekend`
+        break
+        default: return `${day} is weekday`
+    }
+}
+// console.log(ifWeekday('Monday'))
