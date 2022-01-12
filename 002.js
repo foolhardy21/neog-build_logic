7e6ea0fb2946b06bd845507a8c98a44945b3ca7c
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-01-12 15:30:34 +0530

    002 medium complete

--- 002.js
+++ 002.js
@@ -31,3 +31,37 @@ const typeOfTriangle = (a,b,c) => {
     }
 }
 console.log(typeOfTriangle(30,30,90))
+
+
+//Medium
+//1
+const arrLength = arr => arr.length
+console.log(arrLength([1,2,123,4,4,5]))
+
+//2
+const itemInArr = (arr,item) => arr.indexOf(item)
+console.log(itemInArr([1,2,3,4,5],5))
+
+//3
+const replaceinArr = (arr,num1,num2) => arr.map(num => {
+    if(num === num1) return num2
+    else return num
+})
+console.log(replaceinArr([1,5,3,5,6,8],5,10))
+
+//4
+const mergeArrays = (arr1, arr2) => [...arr1,...arr2]
+console.log(mergeArrays([1,2],[3,4]))
+
+//5
+const charAt = (str, index) => str[index]
+console.log(charAt('neogCamp',4))
+
+//6
+const compareDates = (d1, d2) => {
+    const d1Time = new Date(d1).getTime()
+    const d2Time = new Date(d2).getTime()
+    if(d1Time <= d2Time) return d1
+    else return d2
+} 
+console.log(compareDates('05/01/2021','04/01/2021'))
